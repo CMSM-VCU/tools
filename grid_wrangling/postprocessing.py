94a36a4785dbbb0ae2e103cc8a5a0c0d824a0718
watsonkh <watsonkh@vcu.edu>
2021-12-06 15:36:29 -0500

    Postprocessing stuffs

---
+++ grid_wrangling/postprocessing.py
@@ -0,0 +1,83 @@
+import pyvista as pv
+import numpy as np
+import pandas as pd
+import os
+
+
+
+filename = r"C:/Users/watsonkh/Desktop/Yarn_Layers_Results/0PNF/case_0/case_0_0PNF.h5"
+folder = filename[:filename.rfind("/")+1]
+
+test_image = False
+exaggeration = 1.0
+
+
+image_size = (600, 1000)
+
+dataset = "dmg"
+dataset_display_name = "Damage"
+
+plotter = pv.Plotter(off_screen=True, window_size=image_size)
+
+
+# Front view
+camera_position = np.array([0,0,1])
+camera_look_at = np.array([0,0,0])
+camera_up = np.array([0,1,0])
+camera_scale = 2
+
+
+
+def render_image(hf, timestep):
+    print(f"Plotting timestep {timestep}")
+    # Get point positions
+    position = np.array(hf.loc[timestep, ("x1", "x2", "x3")])
+    displacement = np.array(hf.loc[timestep, ("u1", "u2", "u3")])
+    displaced_position = position + (displacement * exaggeration)
+
+    # Create pyvista object
+    polydata = pv.PolyData(displaced_position)
+    polydata[dataset_display_name] = hf.loc[timestep, dataset]
+
+    # Pyvista plotting options
+    plotter.add_mesh(polydata,scalar_bar_args={'title': dataset_display_name}, cmap='plasma')
+    plotter.show_axes()
+
+
+    plotter.enable_parallel_projection()
+    plotter.set_position(camera_position)
+    plotter.set_focus(camera_look_at)
+    plotter.set_viewup(camera_up)
+    plotter.camera.parallel_scale = camera_scale
+
+    plotter.add_text(f"{timestep}")
+    plotter.enable_anti_aliasing()
+    
+    # plotter.show()
+    plotter.screenshot(f"{folder}{dataset_display_name}/{timestep}.png",
+                        transparent_background=True,
+                        window_size=image_size)
+    plotter.clear()
+
+
+
+
+if __name__ == "__main__":
+    print("Reading in data...")
+    hf = pd.read_hdf(filename, key="data")
+    print("Data has been read")
+    timesteps = list(hf.index.levels[0])
+    data = list(hf.columns)
+    print(f"Datasets: {data}")
+
+    try:
+        os.mkdir(f"{folder}{dataset_display_name}")
+    except FileExistsError:
+        pass
+
+    if test_image:
+        for timestep in timesteps[-1:]:
+            render_image(hf, timestep)
+    else:
+        for timestep in timesteps:
+            render_image(hf, timestep)
